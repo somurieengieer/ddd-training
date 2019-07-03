85d66b0bf8f535e08acb30fae710ef7b74b1979f
somurie06 <somurie06@gmail.com>
2019-07-04 00:47:50 +0900

    予約に属性を追加

--- src/com/example/domain/Reservation.java
+++ src/com/example/domain/Reservation.java
@@ -4,4 +4,14 @@ package com.example.domain;
  * 予約
  */
 public class Reservation {
+
+    // TODO: 一部屋一泊のみの予約でとりあえず書いてみる
+
+    Season season;
+
+    RoomType roomType;
+
+    RoomCapacity roomCapacity;
+
+    RoomGuests roomGuests;
 }
