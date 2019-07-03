34da01c2699d1be2e9c6856334f7f238f1c9d2e5
somurie06 <somurie06@gmail.com>
2019-07-04 00:29:41 +0900

    予約、時期を追加

---
+++ src/com/example/domain/Reservation.java
@@ -0,0 +1,7 @@
+package com.example.domain;
+
+/**
+ * 予約
+ */
+public class Reservation {
+}
---
+++ src/com/example/domain/Season.java
@@ -0,0 +1,17 @@
+package com.example.domain;
+
+/**
+ * 時期
+ */
+public enum Season {
+    /** 再繁忙期 */
+    VeryBusy,
+    /** 繁忙期 */
+    Busy,
+    /** 通常期 */
+    Normal,
+    /** 閑散期 */
+    Low,
+    /** 最閑散期 */
+    VeryLow
+}
