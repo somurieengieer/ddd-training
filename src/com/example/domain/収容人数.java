810a4d7aa5c33201e6fc1151953a814f07ff55a9
somurie06 <somurie06@gmail.com>
2019-07-05 08:12:39 +0900

    大人一人あたり宿泊料金の計算を実装

--- src/com/example/domain/収容人数.java
+++ src/com/example/domain/収容人数.java
@@ -4,8 +4,34 @@ public enum 収容人数 {
 
     //TODO: 収容人数もenumで実装できるかは未確認（by hiru
 
-    一名一室,
-    二名一室,
-    三名一室,
-    四名一室;
+    一名一室 {
+        public 大人一人あたり宿泊料金 calculater(時期 season) {
+
+            大人一人あたり宿泊料金 priceForAdult = season.calculaterForOneParson(this);
+            return priceForAdult;
+        }
+    },
+    二名一室 {
+        public 大人一人あたり宿泊料金 calculater(時期 season) {
+
+            大人一人あたり宿泊料金 priceForAdult = season.calculaterForTwoParson(this);
+            return priceForAdult;
+        }
+    },
+    三名一室 {
+        public 大人一人あたり宿泊料金 calculater(時期 season) {
+
+            大人一人あたり宿泊料金 priceForAdult = season.calculaterForThreeParson(this);
+            return priceForAdult;
+        }
+    },
+    四名一室 {
+        public 大人一人あたり宿泊料金 calculater(時期 season) {
+
+            大人一人あたり宿泊料金 priceForAdult = season.calculaterForFourParson(this);
+            return priceForAdult;
+        }
+    };
+
+    public abstract 大人一人あたり宿泊料金 calculater(時期 season);
 }
--- src/com/example/domain/部屋タイプ.java
+++ src/com/example/domain/部屋タイプ.java
@@ -3,4 +3,5 @@ package com.example.domain;
 public enum 部屋タイプ {
     一般客室,
     特別室;
+
 }
