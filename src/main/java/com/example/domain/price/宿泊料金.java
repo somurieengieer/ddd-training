776d1579d86086c80fac69aed5a83c7b73d46e79
somurie06 <somurie06@gmail.com>
2019-07-06 07:28:25 +0900

    宿泊料金の名称を変更

---
+++ src/main/java/com/example/domain/price/宿泊料金.java
@@ -0,0 +1,10 @@
+package com.example.domain.price;
+
+public class 宿泊料金 {
+
+    long value;
+
+    public 宿泊料金(long value) {
+        this.value = value;
+    }
+}
--- src/main/java/com/example/domain/room/収容人数.java
+++ src/main/java/com/example/domain/room/収容人数.java
@@ -1,6 +1,6 @@
 package com.example.domain.room;
 
-import com.example.domain.price.大人一人あたり宿泊料金;
+import com.example.domain.price.宿泊料金;
 import com.example.domain.price.時期;
 
 public enum 収容人数 {
@@ -8,33 +8,33 @@ public enum 収容人数 {
     //TODO: 収容人数もenumで実装できるかは未確認（by hiru
 
     一名一室 {
-        public 大人一人あたり宿泊料金 calculater(時期 season) {
+        public 宿泊料金 calculater(時期 season) {
 
-            大人一人あたり宿泊料金 priceForAdult = season.calculaterForOneParson(this);
+            宿泊料金 priceForAdult = season.calculaterForOneParson(this);
             return priceForAdult;
         }
     },
     二名一室 {
-        public 大人一人あたり宿泊料金 calculater(時期 season) {
+        public 宿泊料金 calculater(時期 season) {
 
-            大人一人あたり宿泊料金 priceForAdult = season.calculaterForTwoParson(this);
+            宿泊料金 priceForAdult = season.calculaterForTwoParson(this);
             return priceForAdult;
         }
     },
     三名一室 {
-        public 大人一人あたり宿泊料金 calculater(時期 season) {
+        public 宿泊料金 calculater(時期 season) {
 
-            大人一人あたり宿泊料金 priceForAdult = season.calculaterForThreeParson(this);
+            宿泊料金 priceForAdult = season.calculaterForThreeParson(this);
             return priceForAdult;
         }
     },
     四名一室 {
-        public 大人一人あたり宿泊料金 calculater(時期 season) {
+        public 宿泊料金 calculater(時期 season) {
 
-            大人一人あたり宿泊料金 priceForAdult = season.calculaterForFourParson(this);
+            宿泊料金 priceForAdult = season.calculaterForFourParson(this);
             return priceForAdult;
         }
     };
 
-    public abstract 大人一人あたり宿泊料金 calculater(時期 season);
+    public abstract 宿泊料金 calculater(時期 season);
 }
