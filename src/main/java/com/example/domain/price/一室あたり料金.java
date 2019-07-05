9234d24eb7715278972eacdf5e85dc17e517527c
somurie06 <somurie06@gmail.com>
2019-07-05 23:00:09 +0900

    パッケージ構成を変更

--- src/main/java/com/example/domain/price/一室あたり料金.java
+++ src/main/java/com/example/domain/price/一室あたり料金.java
@@ -1,4 +1,4 @@
-package com.example.domain;
+package com.example.domain.price;
 
 public class 一室あたり料金 {
 
--- src/main/java/com/example/domain/reservation/同部屋に宿泊する客一覧.java
+++ src/main/java/com/example/domain/reservation/同部屋に宿泊する客一覧.java
@@ -1,4 +1,4 @@
-package com.example.domain;
+package com.example.domain.reservation;
 
 import java.util.List;
 
