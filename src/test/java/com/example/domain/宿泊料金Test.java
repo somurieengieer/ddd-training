45465adeb5593be3991a4fa7926417ca82445419
setoguchi <setoguchi.takuma@gmail.com>
2019-07-06 10:19:22 +0900

    テストを修正

--- src/test/java/com/example/domain/宿泊料金Test.java
+++ src/test/java/com/example/domain/宿泊料金Test.java
@@ -3,6 +3,7 @@ package com.example.domain;
 import com.example.domain.fee.capacity.収容人数区分;
 import com.example.domain.fee.season.時期区分;
 import com.example.domain.fee.宿泊料金;
+import com.example.domain.fee.料金;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +14,7 @@ class 宿泊料金Test {
 
     @Test
     void 料金() {
-        宿泊料金 stay = new 宿泊料金(時期区分.繁忙期, 収容人数区分.二名一室);
-        assertEquals(new com.example.domain.price.宿泊料金(2800).toString(), stay.大人料金().toString());
+        宿泊料金 fee = new 宿泊料金(時期区分.繁忙期, 収容人数区分.二名一室);
+        assertEquals(new 料金(2800), fee.大人料金());
     }
 }
