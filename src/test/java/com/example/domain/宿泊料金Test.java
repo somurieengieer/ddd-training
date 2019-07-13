80e7af4b45f1307e38c90303b43ccc69f75e3bae
somurie06 <somurie06@gmail.com>
2019-07-13 16:39:42 +0900

    単体テストを修正

--- src/test/java/com/example/domain/宿泊料金Test.java
+++ src/test/java/com/example/domain/宿泊料金Test.java
@@ -4,6 +4,7 @@ import com.example.domain.fee.capacity.収容人数区分;
 import com.example.domain.fee.season.時期区分;
 import com.example.domain.fee.宿泊料金;
 import com.example.domain.fee.料金;
+import com.example.domain.room.客室区分;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,7 +15,7 @@ class 宿泊料金Test {
 
     @Test
     void 料金() {
-        宿泊料金 fee = new 宿泊料金(時期区分.繁忙期, 収容人数区分.二名一室);
+        宿泊料金 fee = new 宿泊料金(時期区分.繁忙期, 収容人数区分.二名一室, 客室区分.一般客室);
         assertEquals(new 料金(2800), fee.大人料金());
     }
 }
